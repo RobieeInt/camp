697698633faf463ccda66197ddb5eddf3d498595
RobieeInt <74244073+RobieeInt@users.noreply.github.com>
2021-11-28 18:15:14 +0700

    edit2gaib

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Camp;
 use App\Models\CampBenefit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Checkout;
+
 
 class HomeController extends Controller
 {
@@ -26,6 +28,16 @@ class HomeController extends Controller
                 return redirect(route('user.dashboard'));
                 break;
         }
+    }
+
+    public function dashboarddata() {
+        $camps = Camp::with('Campbenefit')->get(); //ambil array pertama
+        // $campss = Camp::with('Campbenefit')->get(); //ambil array pertama
+        // return $camps;
+        return view('welcome', [
+            'camps' => $camps,
+            // 'campss' => $campss
+        ]);
     }
 
 
